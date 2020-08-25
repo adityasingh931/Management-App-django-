b51df8145ecee02786a5eb2f0c29d16cb18be50e
“adityasingh931” <aditya.singh@ezedox.com>
2020-08-25 22:33:56 +0530

    django project

--- org_manager/models.py
+++ org_manager/models.py
@@ -6,10 +6,10 @@ class Manager(models.Model):
     first_name = models.CharField(max_length=100, null=True,  blank=True)
     last_name = models.CharField(max_length=100, null=True,  blank=True)
     email = models.EmailField(unique=True)
-    password = models.CharField(max_length=250,blank=False,null=False)
-    address = models.TextField(blank=True,null=True,unique=False)
+    password = models.CharField(max_length=250, blank=False, null=False)
+    address = models.TextField(blank=True, null=True, unique=False)
     dob = models.DateField(null=False, blank=False)
-    company = models.TextField(blank=True,null=True,unique=False)
+    company = models.TextField(blank=True, null=True, unique=False)
 
     @property
     def representation(self):
@@ -28,10 +28,10 @@ class Employee(models.Model):
     first_name = models.CharField(max_length=100, null=True,  blank=True)
     last_name = models.CharField(max_length=100, null=True,  blank=True)
     email = models.EmailField(unique=True)
-    password = models.CharField(max_length=250,blank=Falsenull=False)
-    address = models.TextField(blank=Truenull=True,unique=False)
+    password = models.CharField(max_length=250, blank=False, null=False)
+    address = models.TextField(blank=True, null=True, unique=False)
     dob = models.DateField(null=False, blank=False)
-    company = models.TextField(blank=True,null=True,unique=False)
+    company = models.TextField(blank=True, null=True, unique=False)
     mobile = models.IntegerField(null=False, blank=False)
 
     @property
